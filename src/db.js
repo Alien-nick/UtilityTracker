1176ee0ed6d66531ce65a6e3b205a467e3bc273e
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-05-23 19:34:16 -0400

    Add src

---
+++ src/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+var mongoDB = `${process.env.MONGO}`
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// Define Schema
+var Schema = mongoose.Schema
+var gplNotifier = new Schema({
+  time: {
+    type: Date, default: new Date()
+  },
+  status: String
+})
+
+module.exports = mongoose.model('gplNotifier', gplNotifier)

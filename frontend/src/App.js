19a041fe05e55eba72e0f4ef2bf61e03d81a74ee
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-05-27 05:58:33 -0400

    Remove react template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="container">
+          <h2>Utility App</h2>
+        </div>
+        <Route path="/backend" exact component={backendForm}/>
+        <Route path="/charts" exact component={charts}/>
+        <Route path="/cards" exact component={cards}/>
+        <Route path="/logs" exact component={logs}/>
+      </Router>
   );
 }
 

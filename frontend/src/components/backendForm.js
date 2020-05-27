822b365e295c36e999c57c9d0d7150ce36f2db9a
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-05-27 05:58:41 -0400

    Add components

---
+++ frontend/src/components/backendForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditTodo extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to the form Component!!</p>
+            </div>
+        )
+    }
+}

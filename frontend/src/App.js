e0929b82230c41393f533482ef64e6cc4aa8b8e2
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-05-29 19:42:51 -0400

    Import bulma | Fix navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import "bulma/css/bulma.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import backendForm from './components/backendForm';
 import charts from './components/charts';
 import cards from './components/cards';
 import logs from './components/logs';
 import logo from './logo.svg';
+import "bulma/css/bulma.css";
 
 function App() {
   return (
     <Router>
-      <div>
-          <nav class="navbar is-link" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-              <a class="navbar-item">
-                <img src={logo} alt="Utility Notifier" width="50" height="50"/>
-              </a>
-              <span class="navbar-item has-text-weight-bold">Utility Notifier</span>
+      <div class="">
+          <nav class="navbar is-link is-active" role="navigation" aria-label="main navigation">
+            <div class="navbar-start">
+              <Link to="#" className="navbar-item"><span class="has-text-weight-bold has-white-text">Utility Notifer </span></Link>
+            </div>
+            <div class="navbar-end">
               <Link to="/logs" className="navbar-item"><span class="has-text-weight-bold">Gpl Logs </span></Link>
               <Link to="/backend" className="navbar-item"><span class="has-text-weight-bold">Settings </span></Link>
-            </div>
+            </div> 
           </nav>
           <br/>
           <Route path="/backend" component={backendForm}/>

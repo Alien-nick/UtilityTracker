77e1fa6c89436b45c9697244a969bc0149582074
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-05-29 20:45:56 -0400

    Add power cards

---
+++ frontend/src/components/powerCards.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default class PowerCards extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                <div class="columns">
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="content">
+                                    Hours up
+                                <br/>
+                                    <span>3hrs</span>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="content">
+                                    Outages past week
+                                <br/>
+                                <time>5</time>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="content">
+                                    Uptime
+                                <br/>
+                                <time datetime="2016-1-1">50%</time>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="content">
+                                    Temperature
+                                <br/>
+                                    <span>137C(129 F)</span>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <br/>
+                <div>
+                </div>
+                <br/>
+            </div>
+        )
+    }
+}
+
+export { PowerCards }

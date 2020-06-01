8e6e4d716c0d4244705aeb01d1da0e58a4d276b8
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-05-31 22:21:06 -0400

    Add endpoint for charts

--- frontend/src/components/powerCharts.js
+++ frontend/src/components/powerCharts.js
@@ -1,40 +1,64 @@
 import React, { Component } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
+import axios from 'axios';
+import moment from 'moment';
 
 export default class PowerCharts extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            irregularities: {
-                labels: [
-                    '2 Min ago', '3 Min ago', '4 Min ago', '5 Min ago', '6 Min ago', '7 Min ago'
-                ],
-                datasets: [{
-                    label: 'Volts',
-                    data: [
-                        3.3, 3.2, 3.4, 2.7, 2, 2.9
-                    ],
-                    backgroundColor: [
-                        'rgba(255,99,132,0.6)'
-                    ]
-                }]
-            },
-            powerOutage: {
-                labels: [
-                    '2 Min ago', '3 Min ago', '4 Min ago', '5 Min ago', '6 Min ago', '7 Min ago'
-                ],
-                datasets: [{
-                    label: 'PowerOutage',
-                    data: [
-                        1, 0, 1, 0, 1, 1, 0
-                    ],
-                    backgroundColor: [
-                        'rgba(255,77,66,0.6)'
-                    ]
-                }]
-            }
+            irregularities: {},
+            powerOutage: {}
         }
+    }
+
+    componentWillMount() {
+        this.getIrregularities();
+    }
+
+    getIrregularities() {
+        axios.get('http://127.0.0.1:4002/powerlog/charts')
+            .then(res => {
+                let data = res.data
+                let volt = []
+                let timeAgo = []
+                let status = []
+
+                data.forEach(record => {
+                    volt.push(record.voltage)
+                    var time = moment(record.time).format("MMMM D YYYY, H:mm:ss")
+                    var timeFrom = moment(time).fromNow()
+                    timeAgo.push(timeFrom)
+                    status.push(record.power)
+                })
+
+                this.setState({
+                    irregularities:{
+                        labels: timeAgo,
+                        datasets: [{
+                            label: 'Volts',
+                            data: volt,
+                            backgroundColor: [
+                                'rgba(255,99,132,0.6)'
+                            ]
+                        }]
+                    },
+                    powerOutage: {
+                        labels: timeAgo,
+                        datasets: [{
+                            label: 'Stable',
+                            data: status,
+                            backgroundColor: [
+                                'rgba(255,77,66,0.6)'
+                            ]
+                        }]
+                    }
+                })
+
+            }).catch((error) => {
+                console.log(error)
+            })
     }
 
     render() {

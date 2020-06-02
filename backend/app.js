a2b82dbd7168eaeabce4192955766eda22554ceb
Nicholas Seetaram <nicholas@intellectstorm.com>
2020-06-02 08:29:46 -0400

    Fix with eslint

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,15 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const PORT = 4002;
-const router = express.Router()
+const PORT = 4002
 var gplRouter = require('./routes/gpl')
-
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use("/powerlogs", gplRouter)
-app.use("/powerlogs/summary", gplRouter)
-app.use("/powerlogs/charts", gplRouter)
+app.use('/powerlogs', gplRouter)
+app.use('/powerlogs/summary', gplRouter)
+app.use('/powerlogs/charts', gplRouter)
 
 app.listen(PORT, () => {
-    console.log('Server is running on: ' + PORT)
+  console.log('Server is running on: ' + PORT)
 })
